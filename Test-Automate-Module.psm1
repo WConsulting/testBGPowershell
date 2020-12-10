0007193113f718868cd5c3159ca128f628f62f08
WConsulting <69166467+WConsulting@users.noreply.github.com>
2020-12-10 10:28:26 -0500

    Update Test-Automate-Module.psm1

--- Test-Automate-Module.psm1
+++ Test-Automate-Module.psm1
@@ -797,7 +797,7 @@ BEGIN
         Write-Verbose "$($AutomateURL) is Active"
     }
     Catch {
-        Write-Host "The Automate Server Parameter Was Not Entered or InaccessibleERROR999" -ForegroundColor Red
+        Write-Host "The Automate Server Parameter Was Not Entered or InaccessibleERROR99925" -ForegroundColor Red
         Write-Host "Help: Get-Help Push-Automate -Full"
         Break
         }
