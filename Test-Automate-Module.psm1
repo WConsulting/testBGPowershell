9f7a5e77ae2a4d8c2641700242f1a19dc9ee418c
WConsulting <69166467+WConsulting@users.noreply.github.com>
2020-12-10 10:24:53 -0500

    Update Test-Automate-Module.psm1

--- Test-Automate-Module.psm1
+++ Test-Automate-Module.psm1
@@ -824,7 +824,7 @@ PROCESS
     $Time = Date
     $CheckAutomateWinRM = {
         Write-Verbose "Invoke Confirm-Automate -Silent"
-        Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/WConsulting/testBGPowershell/e84c0bb0036d4aa3258888efbc8f1ed4570a7c98/Test-Automate-Module.psm1')
+        Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/WConsulting/testBGPowershell/main/Test-Automate-Module.psm1')
         Confirm-Automate -Silent
         Write $Global:Automate
     }
@@ -835,10 +835,10 @@ PROCESS
         $Force = $Args[3]
         $Silent = $Args[4]
         $Transcript = $Args[5]
-        Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/WConsulting/testBGPowershell/e84c0bb0036d4aa3258888efbc8f1ed4570a7c98/Test-Automate-Module.psm1')
+        Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/WConsulting/testBGPowershell/main/Test-Automate-Module.psm1')
         Install-Automate -Server $Server -LocationID $LocationID -Token $Token -Transcript
     }
-    $WMICMD = 'powershell.exe -Command "Invoke-Expression(New-Object Net.WebClient).DownloadString(''https://raw.githubusercontent.com/WConsulting/testBGPowershell/e84c0bb0036d4aa3258888efbc8f1ed4570a7c98/Test-Automate-Module.psm1''); '
+    $WMICMD = 'powershell.exe -Command "Invoke-Expression(New-Object Net.WebClient).DownloadString(''https://raw.githubusercontent.com/WConsulting/testBGPowershell/main/Test-Automate-Module.psm1''); '
     $WMIPOSH = "Install-Automate -Server $Server -LocationID $LocationID -Token $Token -Transcript"
     $WMIArg = Write-Output "$WMICMD$WMIPOSH"""
     $WinRMConectivity = "N/A"
@@ -850,7 +850,7 @@ PROCESS
     # Now Trying WinRM 
     If ($Computer -eq $env:COMPUTERNAME) {
         Write-Verbose "Installing Automate on Local Computer - $Computer"
-        Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/WConsulting/testBGPowershell/e84c0bb0036d4aa3258888efbc8f1ed4570a7c98/Test-Automate-Module.psm1')
+        Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/WConsulting/testBGPowershell/main/Test-Automate-Module.psm1')
         Install-Automate -Server $Server -LocationID $LocationID -Token $Token -Show:$Show -Transcript:$Transcript
     } Else {        # Remote Computer
         If (!$Silent) {Write-Host "$($Time) - Now Checking $($COMPUTER)"}
